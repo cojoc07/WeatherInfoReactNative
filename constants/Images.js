81c9e0e2dc6b2e9bc9e98a0570d967409d76ebf4
Stefan-George Cojocaru <cojoc07@gmail.com>
2020-12-30 11:22:35 +0200

    corrected asset paths
    corrected asset paths

--- constants/Images.js
+++ constants/Images.js
@@ -1,35 +1,36 @@
 export const IMAGES = [
   {
-    file: require("../assets/images/clear-day.png"),
+    file: require("../../assets/images/clear-day.png"),
   },
   {
-    file: require("../assets/images/clear-night.png"),
+    file: require("../../assets/images/clear-night.png"),
   },
   {
-    file: require("../assets/images/cloudy.png"),
+    file: require("../../assets/images/cloudy.png"),
   },
   {
-    file: require("../assets/images/fog.png"),
+    file: require("../../assets/images/fog.png"),
   },
   {
-    file: require("../assets/images/partly-cloudy-day.png"),
+    file: require("../../assets/images/partly-cloudy-day.png"),
   },
   {
-    file: require("../assets/images/partly-cloudy-night.png"),
+    file: require("../../assets/images/partly-cloudy-night.png"),
   },
   {
-    file: require("../assets/images/rain.png"),
+    file: require("../../assets/images/rain.png"),
   },
   {
-    file: require("../assets/images/sleet.png"),
+    file: require("../../assets/images/sleet.png"),
   },
   {
-    file: require("../assets/images/snow.png"),
+    file: require("../../assets/images/snow.png"),
+  },
+
+  {
+    file: require("../../assets/images/sunny.png"),
   },
   {
-    file: require("../assets/images/sunny.png"),
-  },
-  {
-    file: require("../assets/images/wind.png"),
+    file: require("../../assets/images/wind.png"),
   },
 ];
